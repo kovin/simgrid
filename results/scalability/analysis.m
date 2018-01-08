af1cab49d2148de6191e28c4569be41cd2f1f075
Kevin Piotrkowski <kevin.piotrkowski@avature.net>
2018-01-08 00:15:58 -0300

    Fit results of first pre-experiment with quadratic function

--- results/scalability/analysis.m
+++ results/scalability/analysis.m
@@ -1,7 +1,17 @@
-[A,delimiterOut,numOfHeaders] = importdata('/home/kevin/exactas/simgrid/results/scalability/data_for_analysis.txt')
-nodes = A.data(:, 1)
-cRSS = A.data(:, 2)
-javaRSS = A.data(:, 3)
-plot(nodes,[cRSS, javaRSS])
+[A,delimiterOut,numOfHeaders] = importdata('/home/kevin/exactas/simgrid/results/scalability/data_for_analysis.txt');
+nodes = A.data(:, 1);
+cRSS = A.data(:, 2);
+javaRSS = A.data(:, 3);
+n=2;
+
+
+pCRSS = polyfit(nodes, cRSS,n);
+fCRSS = polyval(pCRSS, nodes);
+
+pJavaRSS = polyfit(nodes, javaRSS, n);
+fJavaRSS = polyval(pJavaRSS, nodes);
+
+plot(nodes,[cRSS, fCRSS, javaRSS, fJavaRSS]);
 xlabel(A.colheaders(1,1));
-legend(A.colheaders(2:3))
+%legend(A.colheaders(2:3));
+legend(['C';'C fit with ^2';'Java';'Java fit with ^2']);
